d213f5bb92686f5afaa302fba764d4c6364d16db
ersaathayainsyira <ersaathayainsyira.1911060059@mail.darmajaya.ac.id>
2022-04-12 22:04:01 +0700

    commit

--- backend/data-structure/assignment/scoreboard/main.go
+++ backend/data-structure/assignment/scoreboard/main.go
@@ -18,7 +18,27 @@ func (s Scores) Len() int {
 }
 
 func (s Scores) Less(i, j int) bool {
-	return false // TODO: replace this
+
+	// hitung score
+	score1 := s[i].Correct*4 - s[i].Wrong
+	score2 := s[j].Correct*4 - s[j].Wrong
+
+	// kalau score1 lebih besar, return true 
+	// kalau score1 lebih kecil, return false
+	if score1 > score2 {
+		return true
+	} else if score1 < score2 {
+		return false
+	}
+
+	// Yang Jumlah Benar-nya lebih tinggi akan diurutkan di atas.
+	if s[i].Correct > s[j].Correct {
+		return true
+	} else if s[i].Correct < s[j].Correct {
+		return false
+	}
+
+	return false
 }
 
 func (s Scores) Swap(i, j int) {
@@ -26,7 +46,7 @@ func (s Scores) Swap(i, j int) {
 }
 
 func (s Scores) TopStudents() []string {
-	sort.Sort(s)
+	sort.Sort(s) // sorting
 	names := []string{}
 	for _, score := range s {
 		names = append(names, score.Name)
@@ -37,7 +57,7 @@ func (s Scores) TopStudents() []string {
 func main() {
 	scores := Scores([]Score{
 		{"Levi", 3, 2, 2},
-		{"Agus", 3, 4, 0},
+		{"Agus", 5, 2, 0},
 		{"Doni", 3, 0, 7},
 		{"Ega", 3, 0, 7},
 		{"Anton", 2, 0, 5},
